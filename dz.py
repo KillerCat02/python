d64265aa03d027c50f2a139b16228f06bfee6e5e
KillerCat02 <36854919+KillerCat02@users.noreply.github.com>
2020-01-25 13:16:39 +0300

    dz
    dz

---
+++ dz.py
@@ -0,0 +1,3 @@
+print("https://www.codewars.com/kata/5861d28f124b35723e00005e")
+print ("https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a")
+print ("https://www.codewars.com/kata/5545f109004975ea66000086")
